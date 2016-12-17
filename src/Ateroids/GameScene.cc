0d435734faecb530a2882f8ba2bd158ddfedc0c8
Valen <Valen@DESKTOP-T65HDMD>
2016-12-17 14:03:03 +0100

    Vamos a probar otra vez ...

--- src/Ateroids/GameScene.cc
+++ src/Ateroids/GameScene.cc
@@ -4,14 +4,16 @@
 #include "MenuScene.hh"
 #include "LevelScene.hh"
 #include "GameScene.hh"
+#include "Ship.hh"
 
 using namespace Logger;
 
 GameScene::GameScene(void){
 	m_background = { { 0, 0, W.GetWidth(), W.GetHeight() }, ObjectID::BG_00 };
-	ship.image = { {300,200, 20, 20}, ObjectID::PLAYER };
-	
+	player.image = { { 300,200, 20, 20 }, ObjectID::PLAYER };
 	m_score = 0;
+	limit = 5;
+	//ship.image = { {300,200, 20, 20}, ObjectID::PLAYER };
 }
 
 GameScene::~GameScene(void) {
@@ -27,12 +29,29 @@ void GameScene::Update(void) {
 	static MouseCoords mouseCoords(0, 0);
 	if (IM.IsMouseDown<MOUSE_BUTTON_LEFT>()) {
 		mouseCoords = IM.GetMouseCoords();
+		AddAST();
 	}
+	if (IM.IsKeyDown<KEY_BUTTON_LCTRL>()) {
+		DeleteAST();
+	}
+	if (frames >= 20) {
+		for (int i = 0; i < Asts.size(); i++) {
+			Asts[i].Movement();
+			Asts[i].Update();
+		}
+		frames = 0;
+	}
+	frames++;
 }
 
 void GameScene::Draw(void) {
 	m_background.Draw();
-	ship.image.Draw();
+	player.image.Draw();
+	//ship.image.Draw();
+
+	for (int i = 0; i < Asts.size(); i++) {
+		Asts[i].asteroid_image.Draw();
+	}
 
 	GUI::DrawTextSolid<FontID::ASTEROIDS>("score: " + std::to_string(m_score),
 	{ W.GetWidth() >> 2, int(W.GetHeight()*.9f), 1, 1 },
