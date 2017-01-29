d6f8a18264c8315824f0ec7ff0e522423fb68f5d
Valen <Valen@DESKTOP-86KCID4>
2017-01-29 17:13:53 +0100

    Bug Ranking solved

--- src/Ateroids/Ranking.hh
+++ src/Ateroids/Ranking.hh
@@ -31,13 +31,36 @@ public:
 	void ReadFile() { 
 		ifstream fentrada("../../res/ranking/Ranking24.dat", ios::in | ios::binary); //Abre archivo
 		if (fentrada.fail())cout << "<WARNING> Binary file can not be opened" << endl; //Lanza error si no se ha abierto bien.
-		fentrada.read(reinterpret_cast<char *>(&ranking), sizeof(ranking)); //Lee el contenido y se almacena en el contenedor.
+
+		Player playerAux[10];
+
+		for (int i = 0; i < 10; i++) {
+			std::getline(fentrada, playerAux[i].playerName, '\0');
+			fentrada.read((char*)&playerAux[i].playerScore, sizeof(playerAux[i].playerScore));
+		}
+
 		fentrada.close(); //Cierra el archivo.
+		for (int i = 0; i < 10; i++) {
+			ranking[i].playerName = playerAux[i].playerName;
+			ranking[i].playerScore = playerAux[i].playerScore;
+		}
 	}
 	void WriteFile() { 
 		ofstream fsalida("../../res/ranking/Ranking24.dat", ios::out | ios::binary); //Abre archivo
 		if (fsalida.fail())cout << "<WARNING> Binary file can not be opened" << endl; //Lanza error si no se ha abierto bien.
-		fsalida.write(reinterpret_cast<char *>(&ranking), sizeof(ranking)); //Sobreescribe el archivo con el contenido del array.
+
+		Player playerAux[10];
+
+		for (int i = 0; i < 10; i++) {
+			playerAux[i].playerName = ranking[i].playerName;
+			playerAux[i].playerScore = ranking[i].playerScore;
+		}
+		for (int i = 0; i < 10; i++) {
+			fsalida.write(playerAux[i].playerName.c_str(), playerAux[i].playerName.size());
+			fsalida.write("\0", sizeof(char));
+			fsalida.write(reinterpret_cast<char*>(&playerAux[i].playerScore), sizeof(playerAux[i].playerScore));
+		}
+
 		fsalida.close(); //Cierra el archivo.
 	}
 	void OrderArray(Player aPlayer) { 
